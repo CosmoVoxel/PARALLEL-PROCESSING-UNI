de9d154933cf1fa7de679624ecddb0a8c0da097d
Wladyslaw <uladzislau.pastushok@student.put.poznan.pl>
2025-04-20 13:05:24 +0200

    Fix incorrect loop behavior in K1. LOOP break

--- prime1.c
+++ prime1.c
@@ -33,6 +33,7 @@ int main(int argc, char *argv[]) {
         if (primeArray[i]) {
             for (int j = i * i; j < sqrtN; j += i) {
                 primeArray[j] = false;
+                break;
             }
         }
     }
